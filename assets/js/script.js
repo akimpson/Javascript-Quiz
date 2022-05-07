94f42e7aed6d50c92f1a01d69aca0b9eedbbaa98
Aisha Kimpson <akimp313@gmail.com>
2022-05-07 09:56:14 -0400

    added variables to identify & assign questions & answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,66 @@ buttonAEl = document.getElementById("A");
 buttonBEl = document.getElementById("B");
 buttonCEl = document.getElementById("C");
 buttonDEl = document.getElementById("D");
+
+// Add the score and timer variables
+var score = "0";
+var timer = "60";
+
+// Add variables to identify & assign questions & answers
+var questionNumber = [
+  "Question 1",
+  "Question 2",
+  "Question 3",
+  "Question 4",
+  "Question 5",
+  "Question 6",
+];
+
+var question = [
+  "Which one is NOT a data type in Javascript?",
+  "What is an array?",
+  "What is the difference between a local and global variable?",
+  "Which one is NOT a comparison expression?",
+  "Where is the script tag located in the html file?",
+  "What is the purpose of a for loop?",
+];
+
+var aAnswer = [
+  "a. Boolean",
+  "a. A group of events",
+  "a. console.log();",
+  "a. Local variables only contain strings and global variables contain all data types including strings, numbers and boolean data.",
+  "a. &&",
+  "a. In the <head> tag",
+  "a. To cycle through a set of commands a set amount of time",
+];
+
+var bAnswer = [
+  "b. String",
+  "b. A list of elements stored in a single variable. ",
+  "b. window.alert(); ",
+  "b. Local variables are defined outside functions and can be accessed anywhere in the code, whereas global variables can only be accessed in the functions they are defined in.",
+  "b. ||",
+  "b. At the bottom of the <body> tag ",
+  "b. To tell the user the purpose of the code",
+];
+
+var cAnswer = [
+  "c. Variable",
+  "c.A method to do mathematical operations",
+  "c.message();",
+  "c. Global variables can be used by anyone around the world, local variables are only defined on your machine. ",
+  "c. <=",
+  "c. At the top of the <body> tag",
+  "c. To compare values of variables",
+];
+
+var dAnswer = [
+  "d. Number",
+  "d. A type of function",
+  "d. console.dir();",
+  "d. Local variables can only be accessed in the functions they are defined in, whereas global variables are defined outside functions and can be used anywhere in the code.",
+  "d. !!",
+  "d. Before the <head> tag",
+  "d. To set a timer",
+];
